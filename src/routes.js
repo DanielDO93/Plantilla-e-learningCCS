7355a8b859c736c18a2424427ee571492a89b717
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-04-07 12:19:50 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,10 @@ const ObjetivoView = React.lazy(() => import('./views/temarioViews/ObjetivoView'
 const PresentacionView = React.lazy(() => import('./views/temarioViews/PresentacionView'));
 const TemaUnoView = React.lazy(() => import('./views/temarioViews/TemaUnoView'));
 const TemaDosView = React.lazy(() => import('./views/temarioViews/TemaDosView'));
-const ActividadView = React.lazy(() => import('./views/temarioViews/ActividadView'));
-const ConclusionView = React.lazy(() => import('./views/temarioViews/ConclusionView'));
-const RecapitulacionView = React.lazy(() => import('./views/temarioViews/RecapitulacionView'));
-
+const TemaTresView = React.lazy(() => import('./views/temarioViews/TemaTresView'));
+const TemaCuatroView = React.lazy(() => import('./views/temarioViews/TemaCuatroView'));
+const TemaCincoView = React.lazy(() => import('./views/temarioViews/TemaCincoView'));
+const TemaSeisView = React.lazy(() => import('./views/temarioViews/TemaSeisView'));
 
 
 const routes = [
@@ -19,11 +19,12 @@ const routes = [
   { path: '/temarioViews/BienvenidaView', name: 'Bienvenida', component: BienvenidaView },
   { path: '/temarioViews/ObjetivoView', name: 'Objetivo', component: ObjetivoView },
   { path: '/temarioViews/PresentacionView', name: 'Presentación', component: PresentacionView },
-  { path: '/temarioViews/TemaUnoView', name: 'TemaUno', component: TemaUnoView },
-  { path: '/temarioViews/TemaDosView', name: 'TemaUno', component: TemaDosView },
-  { path: '/temarioViews/ActividadView', name: 'Actividad', component: ActividadView },
-  { path: '/temarioViews/ConclusionView', name: 'Conclusion', component: ConclusionView },
-  { path: '/temarioViews/RecapitulacionView', name: 'Recapitulacion', component: RecapitulacionView },
+  { path: '/temarioViews/TemaUnoView', name: 'Definiciones', component: TemaUnoView },
+  { path: '/temarioViews/TemaDosView', name: 'Lineamientos generales de la Metodología P 1', component: TemaDosView },
+  { path: '/temarioViews/TemaTresView', name: 'Lineamientos generales de la Metodología P 2', component: TemaTresView },
+  { path: '/temarioViews/TemaCuatroView', name: 'Lineamientos generales de la Metodología P 3', component: TemaCuatroView },
+  { path: '/temarioViews/TemaCincoView', name: 'Descripción del procedimiento ', component: TemaCincoView },
+  { path: '/temarioViews/TemaSeisView', name: 'Seguimiento de operaciones', component: TemaSeisView },
 
 ];
 
